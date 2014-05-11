41282854363c184ad576b48f20d0a815d41191ec
upwavedev <atom.morgan@gmail.com>
2014-05-11 12:42:10 -0400

    fix indentation

--- js/spa.shell.js
+++ js/spa.shell.js
@@ -18,13 +18,13 @@ spa.shell = (function () {
     configMap = {
       main_html: String()
         + '<div class="spa-shell-head">'
-        	+ '<div class="spa-shell-head-logo"></div>'
-        	+ '<div class="spa-shell-head-acct"></div>'
-        	+ '<div class="spa-shell-head-search"></div>'
+          + '<div class="spa-shell-head-logo"></div>'
+          + '<div class="spa-shell-head-acct"></div>'
+          + '<div class="spa-shell-head-search"></div>'
         + '</div>'
         + '<div class="spa-shell-main">'
-        	+ '<div class="spa-shell-main-nav"></div>'
-        	+ '<div class="spa-shell-main-content"></div>'
+          + '<div class="spa-shell-main-nav"></div>'
+          + '<div class="spa-shell-main-content"></div>'
         + '</div>'
         + '<div class="spa-shell-foot"></div>'
         + '<div class="spa-shell-chat"></div>'
